969d70c70ddaf76fc89c534c3b5c87d84ab73f37
Hristiyan Stefanov <christian.stefanov@outlook.com>
2020-10-31 17:23:19 +0200

    add arrays exercises

---
+++ Arrays - Exercise & More/07.Max_Sequence_Of_Equal_Elements/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07.Max_Sequence_Of_Equal_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            string longestYet = arr[0] + " ";
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                string temp = arr[i] + " ";
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        temp += arr[i] + " ";
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (temp.Length > longestYet.Length)
+                {
+                    longestYet = temp;
+                }
+            }
+
+            Console.WriteLine(longestYet.Trim());
+        }
+    }
+}
---
+++ Arrays - Lab/02.Print_Nums_In_Reverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Print_Nums_In_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+
+            for (int i = n - 1; i > -1; i--)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var num in nums)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
